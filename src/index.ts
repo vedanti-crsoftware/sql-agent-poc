0ed832ab854ae61fcb7c817634285b90f3b142e7
Vedanti Udapure <vedantiudapure@crsoftware.com>
2025-04-21 20:40:47 +0530

    Added routes /optimize_sql and /health to the index.ts

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import optimizerRouter from './routes/optimizer';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use('/optimize_sql',optimizerRouter);
+
+app.get('/health',(_req,res)=> {
+    res.json({status:'healthy'});
+});
