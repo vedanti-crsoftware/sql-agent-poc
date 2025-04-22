97e1f70f3620bb4d1d350d91032ec312e7f7f09e
Vedanti Udapure <vedantiudapure@crsoftware.com>
2025-04-23 01:13:46 +0530

    Added regular expression

--- src/routes/optimizer.ts
+++ src/routes/optimizer.ts
@@ -1,4 +1,4 @@
-import express, {Router, Request, Response} from 'express';
+import express, {Router, Request, Response, response} from 'express';
 import path from 'path';
 import {JSONLoader} from '../utils/jsonUtils';
 import {BedrockService} from '../services/bedrockService';
@@ -24,13 +24,14 @@ router.post('/', async(req : Request,res: any) => {
         const sqlQuery = req.body?.sql_query?.trim();
             console.log("this is the sqlQuery ->",sqlQuery);
             if(!sqlQuery || !sqlQuery.trim().toLowerCase().startsWith('select')){
-               // console.log("OKKKKKK");
+               // console.log("Not OKKKKKK");
             return res.status(400).json({error:'Only SELECT Statemenets are allowed'});
         }
         const prompt = promptTemplate.replace('{sql_query}',sqlQuery);
         console.log("This is final prompt ->>>>>>",prompt);
         const optimized = await bedrockService.invokeModel(prompt);
-        return res.json({optimized_query: optimized});
+        const cleanOptimized = optimized.replace(/\n/g,'');
+        return res.json({optimized_query: cleanOptimized});
     } catch (err) {
         console.error(err);
         return res.status(500).json({error:'Optimization failed'});
